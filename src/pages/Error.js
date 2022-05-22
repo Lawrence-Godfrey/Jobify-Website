3741f17370b74efbef2f295b54e7b05cb6d35909
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-05-22 22:40:54 +0200

    Add empty dashboard, 404 error, and register pages and link to them in App.js

---
+++ src/pages/Error.js
@@ -0,0 +1,14 @@
+
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Error</h1>
+            <p>
+                This page does not exist.
+            </p>
+        </div>
+    );
+}
+
+export default Error;
