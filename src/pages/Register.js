e48090adf515a18f6049b943a6a45dbb514e561a
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-05-28 13:07:13 +0200

    Update register screen to dynamically update to login screen depending on user state

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,59 @@
+import { useState, useEffect } from 'react';
+import { Logo, FormRow, Alert } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import {useAppContext} from "../context/appContext";
 
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+}
 
 const Register = () => {
+    const [values, setValues] = useState(initialState)
+
+    const { isLoading, showAlert } = useAppContext()
+
+    const toggleMember = () => {
+        setValues({ ...values, isMember: !values.isMember })
+    }
+    const handleChange = (e) => {
+        console.log(e.target)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target);
+    }
+
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <Wrapper className='full-page'>
+            <form className='form' onSubmit={onSubmit}>
+                <Logo />
+                <h3>{ values.isMember ? 'Login' : 'Register'}</h3>
+
+                { showAlert && <Alert /> }
+
+                { !values.isMember && (
+                    <FormRow type='text' name='name' value={values.name} handleChange={handleChange} />
+                )}
+
+                <FormRow type='email' name='email' value={values.email} handleChange={handleChange} />
+                <FormRow type='password' name='password' value={values.password} handleChange={handleChange} />
+
+                <button type='submit' className='btn btn-block'>
+                    submit
+                </button>
+
+                <p>
+                    { values.isMember ? 'Not a member?' : 'Already a member?'}
+                    <button type='button' onClick={toggleMember} className='member-btn'>
+                        { values.isMember ? 'Register' : 'Login' }
+                    </button>
+                </p>
+            </form>
+        </Wrapper>
     );
 }
 
