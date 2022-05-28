5b29d4e0dcd1be0f0133e1842c236fb1759e9285
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-05-28 13:06:14 +0200

    Update 404 error page with image and more descriptive text

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,14 +1,18 @@
-
+import { Link } from 'react-router-dom'
+import img from '../assets/images/not-found.svg'
+import Wrapper from '../assets/wrappers/ErrorPage'
 
 const Error = () => {
     return (
-        <div>
-            <h1>Error</h1>
-            <p>
-                This page does not exist.
-            </p>
-        </div>
-    );
+        <Wrapper className='full-page'>
+            <div>
+                <img src={img} alt='not found'/>
+                <h3>Page not found!</h3>
+                <p>We can't seem to find the page you're looking for</p>
+                <Link to='/'>back to safety</Link>
+            </div>
+        </Wrapper>
+    )
 }
 
 export default Error;
