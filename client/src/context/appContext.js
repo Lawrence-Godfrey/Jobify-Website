1a043088b46ecae87d94564a94da669c35de2cd5
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-05-29 18:28:29 +0200

    Move client side code into client dir

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,4 +1,6 @@
 import React, { useState, useReducer, useContext} from "react";
+import reducer from './reducer'
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 
 const initialState = {
     isLoading: false,
@@ -10,10 +12,20 @@ const initialState = {
 const AppContext = React.createContext({})
 
 const AppProvider = ({children}) => {
-    const [state, setState] = useState(initialState)
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const displayAlert = () => {
+        dispatch({ type: DISPLAY_ALERT })
+    }
+
+    const clearAlert = () => {
+        setTimeout(() => {
+            dispatch({ type: CLEAR_ALERT })
+        }, 3000)
+    }
 
     return (
-        <AppContext.Provider value={{ ...state }}>
+        <AppContext.Provider value={{ ...state, displayAlert, clearAlert }}>
             { children }
         </AppContext.Provider>
     )
