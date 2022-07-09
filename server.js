acb55a5f21762289728f1d66615b4e0584065fdd
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-07-09 15:32:38 +0200

    Add express setup and routes to express app

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import notFound from "./middleware/not-found.js";
+import errorHandler from "./middleware/error-handler.js";
+import connectDB from "./db/connect.js";
+import authRouter from './routes/authRoutes.js';
+import jobRouter from './routes/jobRoutes.js';
+
+dotenv.config()
+
 const app = express()
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Welcome!')
-})
+});
+
+app.use('/api/auth', authRouter);
+app.use('/api/jobs', jobRouter);
+
+// Middleware which sends back a 404 error if no route is found
+app.use(notFound)
+
+// Middleware for handling all other errors thrown
+app.use(errorHandler)
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        })
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+start();
