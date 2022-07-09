963d53d03c31b08378718f885cdb98e8c8e64120
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-07-09 15:31:02 +0200

    Add routes for performing CRUD operations on jobs

---
+++ routes/jobRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createJob, deleteJob, updateJob, getAllJobs, showStats } from "../controllers/jobsController.js";
+
+const router = express.Router();
+
+
+router.route("/").post(createJob).get(getAllJobs);
+router.route("/stats").get(showStats);
+router.route("/:id").delete(deleteJob).patch(updateJob);
+
+export default router;
