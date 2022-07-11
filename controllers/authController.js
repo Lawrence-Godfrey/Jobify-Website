062f9c33333691afe12c577862bf1487d9f20b8b
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-07-11 23:53:35 +0200

    Flesh out the registration handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,22 @@
+import User from '../models/user.js';
+import UserSerializer from "../serializers/userSerializer.js";
 
 
-const register = async (req, res) => {
-    res.send('register user')
+const register = async (req, res, next) => {
+    const serializer = new UserSerializer(req.body);
+
+    if (!serializer.isValid()) {
+        return res.status(400).json({
+            message: 'Invalid data',
+            errors: serializer.errors
+        });
+    }
+
+    const user = await serializer.save();
+
+    const token = user.createJWT();
+
+    res.status(201).json({ user: serializer.data(), token });
 }
 
 const login = async (req, res) => {
