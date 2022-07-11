1b8c879366a634125d46a8d0721384693a641a60
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-07-11 23:53:55 +0200

    Create custom api error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,8 +1,15 @@
 const errorHandler = (err, req, res, next) => {
-    console.log(err);
-    res.status(500).json({
-        msg: 'There was an error'
-    });
+    if (err.name === 'ValidationError') {
+        res.status(400).json({
+            message: err._message,
+            errors: err.errors
+        });
+    } else if (err.code && err.code === 11000) {
+        res.status(400).json({
+            message: 'Duplicate field value entered',
+            errors: { ...err.keyValue, message: "Duplicate field value entered" }
+        });
+    }
 }
 
 export default errorHandler;
