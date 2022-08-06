a9924ca280cde6afa66f5f8d64b04b2d2d8dc955
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-08-06 23:33:06 +0200

    Add error handler for unknown internal errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -9,6 +9,17 @@ const errorHandler = (err, req, res, next) => {
             message: 'Duplicate field value entered',
             errors: { ...err.keyValue, message: "Duplicate field value entered" }
         });
+    } else {
+        if (process.env.NODE_ENV === 'development') {
+            res.status(500).json({
+                message: err.message,
+                stack: err.stack
+            });
+        } else {
+            res.status(500).json({
+                message: "Something went wrong"
+            });
+        }
     }
 }
 
