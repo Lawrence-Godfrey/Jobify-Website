ff8806d8e1a4af378b436a5ba68721535b1e8b61
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-08-06 23:34:12 +0200

    Fix and update serializer class

--- serializers/serializer.js
+++ serializers/serializer.js
@@ -11,11 +11,12 @@ class Serializer {
      */
     constructor(requestData, fields, model) {
 
+        this.requestData = requestData;
         this.fields = fields;
         this.model = model;
 
-        this.fieldsToSave = {};
-        this.fieldsToReturn = {};
+        this.fieldsToSave = [];
+        this.fieldsToReturn = [];
         this.displayNames = {};
         this.instance = null;
         this.errors = {};
@@ -23,22 +24,25 @@ class Serializer {
         console.log(this.fields);
 
         // Loop through the request data and only save the fields which are also in the fields object.
-        for (let key in fields) {
-            if (this.fields[key]) {
-                if (this.fields[key]['readOnly'] && this.fields[key]['readOnly'] === true) {
-                        this.fieldsToReturn[key] = requestData[key];
-                        if (this.fields[key]['displayName']) {
-                            this.displayNames[key] = this.fields[key]['displayName'];
-                        }
-                } else if (this.fields[key]['writeOnly'] && this.fields[key]['writeOnly'] === true) {
-                        this.fieldsToSave[key] = requestData[key];
-                } else {
-                    this.fieldsToSave[key] = requestData[key];
-                    this.fieldsToReturn[key] = requestData[key];
-
+        for (let key in this.fields) {
+            if (this.fields[key]['readOnly'] && this.fields[key]['readOnly'] === true) {
+                    this.fieldsToReturn.push(key);
                     if (this.fields[key]['displayName']) {
                         this.displayNames[key] = this.fields[key]['displayName'];
                     }
+            } else if (this.fields[key]['writeOnly'] && this.fields[key]['writeOnly'] === true) {
+                if (this.requestData[key]) {
+                    this.fieldsToSave.push(key);
+                }
+            } else {
+                if (this.requestData[key]) {
+                    this.fieldsToSave.push(key);
+                }
+
+                this.fieldsToReturn.push(key);
+
+                if (this.fields[key]['displayName']) {
+                    this.displayNames[key] = this.fields[key]['displayName'];
                 }
             }
         }
@@ -48,14 +52,20 @@ class Serializer {
      * Get a subset of the fields in the instance which are also in fields.
      * Also use the name in displayNames if possible.
      * @param {Object} sourceObject      The instance to get the subset from
-     * @param {Object} fields         The fields to get from the instance
+     * @param {Array} fields         The fields to get from the instance
      * @param {Object} displayNames  The display names to use for the fields
      * @returns {Object} subset      The subset of the instance
      */
     getSubset(sourceObject, fields, displayNames) {
+
+        displayNames = displayNames || {};
+
         // Get the subset of fields in the instance which are also in fields.
+        // Loop through fields
+
+
         let subset = {};
-        for (let key in fields) {
+        for (let key of fields) {
             if (sourceObject[key]) {
                 if (displayNames[key]) {
                     subset[displayNames[key]] = sourceObject[key];
@@ -70,10 +80,10 @@ class Serializer {
 
     /**
      * Get the validated fields to save.
-     * @returns {AnyObject} validatedData   The validated fields to save
+     * @returns {Object} validatedData   The validated fields to save
      */
     validatedData() {
-        return this.fieldsToSave;
+        return this.getSubset(this.requestData, this.fieldsToSave, this.displayNames);
     }
 
     /**
@@ -98,19 +108,25 @@ class Serializer {
      */
     data() {
         if (this.instance) {
+            // Get fields from this.instance which are in this.fieldsToReturn.
             return this.getSubset(this.instance, this.fieldsToReturn, this.displayNames);
         } else {
-            return this.getSubset(this.validatedData(), this.fieldsToReturn, this.displayNames);
+            throw new Error('No instance to get data from.');
         }
     }
 
     /**
      * Return whether the data is valid or not.
+     * @param {Object} options    Options to use when validating.
+     *                            Can include 'skipRequired' to skip required fields.
      * @returns {boolean}
      */
-    isValid() {
-        this.instance = new this.model(this.fieldsToSave);
-        const errors = this.instance.validateSync();
+    isValid(options) {
+        options = options || {};
+        let skipRequired = options['skipRequired'] || [];
+
+        this.instance = new this.model(this.validatedData());
+        const errors = this.instance.validateSync(["email", "password"]);
         if (errors) {
             // Loop through the error keys and get the message for each.
             for (let key in errors.errors) {
