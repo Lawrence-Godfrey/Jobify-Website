985f8ffbdda603f2f6b73e2eb2975f2a4bc432bb
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2022-11-06 21:23:43 +0200

    Add sidebar functionality with page links

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,10 +1,22 @@
 
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
+import { useAppContext } from "../context/appContext";
+import { NavLinks } from "./index";
 
 
 const BigSidebar = () => {
+    const { showSidebar, toggleSidebar } = useAppContext()
     return <Wrapper>
-        <h4>BigSidebar</h4>
+        <div className={ showSidebar ? "sidebar-container show-sidebar" : "sidebar-container" }>
+            <div className="content">
+                <header>
+                    <Logo />
+                </header>
+                <NavLinks/>
+            </div>
+        </div>
     </Wrapper>
 }
 
--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,27 @@
 
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
+import { useAppContext } from "../context/appContext";
+import { NavLinks } from "./index";
 
 
 const SmallSidebar = () => {
+    const { showSidebar, toggleSidebar } = useAppContext()
     return <Wrapper>
-        <h4>SmallSidebar</h4>
+        <div className={
+            showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"}
+        >
+            <div className="content">
+                <button type="button" className="close-btn" onClick={ toggleSidebar }>
+                    <FaTimes />
+                </button>
+                <header>
+                    <Logo />
+                </header>
+                <NavLinks toggleSidebar={toggleSidebar}/>
+            </div>
+        </div>
     </Wrapper>
 }
 
